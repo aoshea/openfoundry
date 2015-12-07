74de0fc16e2aebf787e61d3da75b8bb3dd416f97
Alastair O'Shea <alastair.oshea@gmail.com>
2015-12-07 17:52:04 +0000

    Image toggler

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -155,7 +155,7 @@ class Slider extends React.Component {
     let dimension, direction, position, coords, fillStyle, handleStyle;
   	let { value, orientation, className, label } = this.props;
 
-    let labelStr = label + " " + value;
+    let labelStr = label + " " + value.toFixed(2);
     
   	dimension = constants.orientation[orientation].dimension;
   	direction = constants.orientation[orientation].direction;
@@ -204,7 +204,7 @@ class FontSlider extends Component {
   
   componentDidMount() {
     this.setState({
-      value: this.props.initial * 100
+      value: this.props.initial
     });
   }
 
@@ -220,16 +220,102 @@ class FontSlider extends Component {
 
   render() {
     return (
-      <div className="of-grid-container"><div className="of-row"><div className="col-2">
+      <div className="col-2">
       <Slider
       value={this.state.value}
       orientation="horizontal"
-      label="size"
+      label={this.props.label}
+      min={this.props.min}
+      max={this.props.max}
+      step={this.props.step}
       onChange={this.handleChange} />
       </div>
-      </div>
-      </div>
     );
+  }
+}
+
+class FontBackgroundToggle extends Component {
+  
+  render() {
+    
+    let size = 32;
+    let viewBox = [0, 0, size, size].join(' ');
+
+    return (
+      <svg xmlns="http://www.w3.org/svg/2000"
+        viewBox={viewBox}>
+        <g id="of-font-toggle-no-image" fill="none" stroke="black">
+	        <circle cx="16" cy="12.8" r="5.5"/>
+          <circle cx="12" cy="19.2" r="5.5"/>
+          <circle cx="20" cy="19.2" r="5.5"/>
+        </g>
+        <g id="of-font-toggle-image">
+          <path fill="#0000FF" d="M14,19.2c0-0.3,0-0.6,0.1-0.8c-2.1-0.7-3.7-2.6-4-4.8c-2.4,0.8-4.1,3-4.1,5.7c0,3.3,2.7,6,6,6
+          c1.5,0,2.9-0.6,4-1.5C14.8,22.6,14,21,14,19.2z"/>
+          <path fill="#00FF00" d="M21.9,13.6c-0.3,2.3-1.9,4.1-4,4.8c0,0.3,0.1,0.5,0.1,0.8c0,1.8-0.8,3.4-2,4.5c1.1,1,2.5,1.5,4,1.5
+          c3.3,0,6-2.7,6-6C26,16.6,24.3,14.4,21.9,13.6z"/>
+          <path fill="#FF0000" d="M16,14.8c1.1-1,2.5-1.5,4-1.5c0.7,0,1.3,0.1,1.9,0.3c0-0.3,0.1-0.5,0.1-0.8c0-3.3-2.7-6-6-6s-6,2.7-6,6
+          c0,0.3,0,0.6,0.1,0.8c0.6-0.2,1.3-0.3,1.9-0.3C13.5,13.2,14.9,13.8,16,14.8z"/>
+          <path fill="#00FFFF" d="M18,19.2c0-0.3,0-0.6-0.1-0.8c-0.6,0.2-1.3,0.3-1.9,0.3s-1.3-0.1-1.9-0.3c0,0.3-0.1,0.5-0.1,0.8
+          c0,1.8,0.8,3.4,2,4.5C17.2,22.6,18,21,18,19.2z"/>
+          <path fill="#FF00FF" d="M14.1,18.4c0.2-1.4,0.9-2.7,1.9-3.6c-1.1-1-2.5-1.5-4-1.5c-0.7,0-1.3,0.1-1.9,0.3
+          C10.4,15.8,12,17.7,14.1,18.4z"/>
+          <path fill="#FFFF00" d="M16,14.8c1,0.9,1.7,2.2,1.9,3.6c2.1-0.7,3.7-2.6,4-4.8c-0.6-0.2-1.3-0.3-1.9-0.3
+          C18.5,13.2,17.1,13.8,16,14.8z"/>
+          <path fill="#FFFFFF" d="M16,14.8c-1,0.9-1.7,2.2-1.9,3.6c0.6,0.2,1.3,0.3,1.9,0.3s1.3-0.1,1.9-0.3C17.7,17,17,15.7,16,14.8z"/>
+        </g>
+      </svg>
+    )
+  }
+}
+
+class FontColourBox extends Component {
+  
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      value: ''
+    };
+  }
+  
+  handleChange(e) {
+    let hex = e.target.value, { onUpdate } = this.props;
+    
+    if (hex.substr(0, 1) !== '#') {
+      hex = '#' + hex;      
+    }
+    
+    hex = hex.substr(0, 7);
+    
+    this.setState({
+      value: hex
+    });
+    
+    onUpdate && onUpdate(hex);
+  }
+  
+  render() {
+    let hexValue = this.state.value.substr(1);
+    
+    return (
+      <div className="of-font-colour-input">
+        <input type="text" value={hexValue} defaultValue={this.props.initial} onChange={this.handleChange} placeholder="000000" />
+      </div>
+    )
+  }
+}
+
+class FontColours extends Component {
+  render() {
+    return (
+      <div className="col-2">
+        <FontColourBox initial={this.props.initial} onUpdate={this.props.onUpdate} />
+        <div className="of-font-background-toggle-container">
+          <FontBackgroundToggle />
+        </div>
+      </div>
+    )
   }
 }
 
@@ -239,17 +325,29 @@ class FontPreviewContainer extends Component {
     super();
     
     this.onUpdateSize = this.onUpdateSize.bind(this);
+    this.onUpdateLineHeight = this.onUpdateLineHeight.bind(this);
+    this.onUpdateLetterSpacing = this.onUpdateLetterSpacing.bind(this);
+    this.onUpdateColour = this.onUpdateColour.bind(this);
     
     this.state = {
-      size: 0
+      size: 0,
+      letterSpacing: 0,
+      lineHeight: 0,
+      color: '#000'
     };
   }
   
   componentDidMount() {
     let fontSize = parseInt(this.props.settings['font-size'], 10);
+    let lineHeight = parseFloat(this.props.settings['line-height'], 10);
+    let letterSpacing = parseFloat(this.props.settings['letter-spacing'], 10);
+    let color = this.props.settings['color'];
     
     this.setState({
-      size: fontSize
+      size: fontSize,
+      lineHeight: lineHeight,
+      letterSpacing: letterSpacing,
+      color: color
     });
   }
   
@@ -259,17 +357,61 @@ class FontPreviewContainer extends Component {
     });
   }
   
+  onUpdateLetterSpacing(value) {
+    this.setState({
+      letterSpacing: value.toFixed(2)
+    });
+  }
+  
+  onUpdateLineHeight(value) {
+    this.setState({
+      lineHeight: value.toFixed(2)
+    });
+  }
+  
+  onUpdateColour(value) {
+    console.log('onUpdateColour', value);
+    this.setState({
+      color: value
+    });
+  }
+  
   render() {
     
     let fontClassName = replaceNonAlphaNumeric(this.props.name).toLowerCase();
+    
     let fontSize = parseInt(this.props.settings['font-size'], 10);
-    let maxFontSize = 100;
-    let fontSizePercentage = fontSize / maxFontSize;
-
-    let fontStyle = { "fontSize" : `${this.state.size}px` };
-        
+    let lineHeight = parseFloat(this.props.settings['line-height'], 10);
+    let letterSpacing = parseFloat(this.props.settings['letter-spacing'], 10);
+    let color = this.props.settings['color'];
+    
+    let maxFontSize = 150;
+    let minFontSize = 9;
+    
+    let maxLineHeight = 2;
+    let minLineHeight = 0.5;
+    
+    let minLetterSpacing = 0;
+    let maxLetterSpacing = 1;
+    
+    let stepFontSize = 1;
+    let stepLetterSpacing = 0.025;
+    let stepLineHeight = 0.05;
+    
+    let fontStyle = { 
+      "fontSize" : `${this.state.size}px`,
+      "letterSpacing": `${this.state.letterSpacing}em`,
+      "lineHeight": `${this.state.lineHeight}em`,
+      "color": `${this.state.color}`      
+    };
+           
     return <div className="of-font-preview-container">
-    <FontSlider initial={fontSizePercentage} onUpdate={this.onUpdateSize} />
+    <div className="of-grid-container"><div className="of-row">
+      <FontSlider label="size" initial={fontSize} max={maxFontSize} step={stepFontSize} min={minFontSize} onUpdate={this.onUpdateSize} />
+      <FontSlider label="leading" initial={lineHeight} max={maxLineHeight} step={stepLineHeight} min={minLineHeight} onUpdate={this.onUpdateLineHeight} />
+      <FontSlider label="kerning" initial={letterSpacing} max={maxLetterSpacing} step={stepLetterSpacing}  min={minLetterSpacing} onUpdate={this.onUpdateLetterSpacing} />
+      <FontColours initial={color} onUpdate={this.onUpdateColour} />
+    </div></div>
     <div data-font={this.props.name} style={fontStyle} className={"of-font-preview-text-container " + fontClassName}>
     {this.props.name} . {this.props.creator}
     </div>
