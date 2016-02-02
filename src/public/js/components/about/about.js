cad20abc2e5c56d449d1cb1ff7e106f64d7290db
Stefan Endress <stefan@stefanendress.com>
2016-02-02 20:08:26 +0100

    minor css fixes

--- src/public/js/components/about/about.js
+++ src/public/js/components/about/about.js
@@ -19,7 +19,7 @@ export default class AboutPage extends Component {
           <div className="of-grid-container">
             <div className="of-row">
               <div className="col-8">
-                <p><a href="http://open-foundry.com/">open-foundry.com</a> — The ‘open’ stands for open-source, free and easily available. The word ‘foundry’ is taken from the ‘type foundry’ and suggests professional quality and industrial heritage. open-foundry was founded by <a href="http://stefanendress.com/">Stefan Endress</a> and <a href="http://alastairoshea.com/">Alastair O’Shea</a>.</p>
+                <p><a href="http://open-foundry.com">open-foundry.com</a> — The ‘open’ stands for open-source, free and easily available. The word ‘foundry’ is taken from the ‘type foundry’ and suggests professional quality and industrial heritage. open-foundry was founded by <a href="http://stefanendress.com">Stefan Endress</a> and <a href="http://alastairoshea.com">Alastair O’Shea</a>.</p>
 
                 <p>If you search for free web fonts on google, one of the first search result is Google Fonts and the link description</p>
               </div>
@@ -47,9 +47,11 @@ export default class AboutPage extends Component {
               <div className="col-8">
                 <p>Tschicholds understanding of typography may now be outdated, but it serves to illustrate  that the choice of the font and its style is never arbitrary and should always be itself.</p>
 
-                <p>Our solution is to offer a carefully controlled number of fonts in the most clear and dynamic way possible. Instead of overwhelming the user, open-foundry provides enough interest and excitement to encourage further exploration.</p>
+                <p>Our solution is to offer a carefully controlled number of fonts in the most clear and dynamic way possible. Instead of overwhelming the user, open-foundry provides enough interest and excitement to encourage further exploration and font development.</p>
 
-                <p>All fonts are distributed under an open-source license and are free to use. Every font provides you with a link to its open-source hosted project, direct download and the site where it was initially found or published. All images used can be found on <a href="commons.wikimedia.org">Wikimedia Commons</a>.</p>
+                <p>All fonts are distributed under an open-source license and are free to use. Every font provides you with a link to its open-source hosted project, direct download and the site where it was initially found or published.</p>
+
+                <p>All images used can be found on <a href="https://commons.wikimedia.org">Wikimedia Commons</a>. Previewed headlines getting pulled automatically from the <a href="http://www.dailymail.co.uk">dailymail.co.uk</a> celebrity section.</p>
 
                 <p><strong>Special Thanks</strong> to Jonathan McKay, Wolfgang Schoeffel, Thomas Traum, Marc Kremers,  Michael Seibert, Adam Rodgers and Tilman Zitzmann for their constructive criticism and always pushing things forward. <svg className="love" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 23.3023"><title>Untitled-4</title><path d="M23.8679,1.9224a7.4057,7.4057,0,0,1,0,10.3987L13.0011,23.3023,2.1321,12.3147a7.4057,7.4057,0,0,1,0-10.3987,6.4188,6.4188,0,0,1,9.1489,0l1.72,1.7349L14.719,1.9224a6.4188,6.4188,0,0,1,9.1489,0"/></svg></p>
 
